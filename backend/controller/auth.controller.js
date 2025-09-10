02896dd4926271fdda12b60485b3541c7aa0119f
Bhavneet singh <bhavneetsingh9002@gmail.com>
2025-09-10 17:39:14 +0530

    deployment

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -64,13 +64,17 @@ export const signin = async (req, res, next) => {
 
 export const signout = async (req, res, next) => {
   try {
-    res.clearCookie("access_token")
+    res.clearCookie("access_token", {
+      httpOnly: true,
+      sameSite: "None", // required if frontend is on different domain
+      secure: true      // required if using HTTPS
+    });
 
     res.status(200).json({
       success: true,
       message: "User logged out successfully",
-    })
+    });
   } catch (error) {
-    next(error)
+    next(error);
   }
-}
+};
